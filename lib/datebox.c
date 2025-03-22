c139c00f637ac73e82d6bf16480724927c48cc77
Alfonso Siciliano <alfsiciliano@gmail.com>
2025-03-22 14:26:15 +0100

    update year

--- lib/datebox.c
+++ lib/datebox.c
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
- * Copyright (c) 2022-2024 Alfonso Sabato Siciliano
+ * Copyright (c) 2022-2025 Alfonso Sabato Siciliano
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
